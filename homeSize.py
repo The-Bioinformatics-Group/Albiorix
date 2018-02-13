fb62d5f2cd02ce965013d5440367b2875bc35557
mtop <mats@topel.se>
2018-02-13 11:42:11 +0100

    Updated the functionallity so that the user is told to decrease disk usage in HOME if >200G is used. The text is displayed on red background.

--- homeSize.py
+++ homeSize.py
@@ -3,4 +3,10 @@
 import subprocess
 import os
 homeSize = subprocess.Popen("du -hs /home/$USER", shell=True, stdout=subprocess.PIPE).stdout.read().split()[0]
-os.system('echo "$(tput setab 4)You have %s of data in your HOME directory.$(tput sgr 0)"' % homeSize)
+
+if homeSize[-1] == "G" and homeSize[:-1] >= "200":
+	os.system('echo "$(tput setab 1)You have %s of data in your HOME directory.$(tput sgr 0)"' % homeSize)
+	os.system('echo "$(tput setab 1)Please decrease your disk usage to >200G.$(tput sgr 0)"')
+
+else:
+	os.system('echo "$(tput setab 4)You have %s of data in your HOME directory.$(tput sgr 0)"' % homeSize)
