9f03abf9178d3757054f19f0a88e2a65de63ca14
Mats Töpel <mats@topel.se>
2018-03-11 18:47:58 +0100

    Corrected spelling error

--- homeSize.py
+++ homeSize.py
@@ -6,7 +6,7 @@ homeSize = subprocess.Popen("du -hs /home/$USER", shell=True, stdout=subprocess.
 
 if homeSize[-1] == "G" and homeSize[:-1] >= "200":
 	os.system('echo "$(tput setab 1)You have %s of data in your HOME directory.$(tput sgr 0)"' % homeSize)
-	os.system('echo "$(tput setab 1)Please decrease your disk usage to >200G.$(tput sgr 0)"')
+	os.system('echo "$(tput setab 1)Please decrease your disk usage to <200G.$(tput sgr 0)"')
 
 else:
 	os.system('echo "$(tput setab 4)You have %s of data in your HOME directory.$(tput sgr 0)"' % homeSize)
