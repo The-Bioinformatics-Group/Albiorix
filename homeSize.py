5bdee6d48ad5a37604e455a99c40e74f2109557c
mtop <mats@topel.se>
2018-09-14 16:05:09 +0200

    Fixed bug in the comparison of the homesize and the threashold

--- homeSize.py
+++ homeSize.py
@@ -1,10 +1,10 @@
-#!/bin/env python
+#!/usr/bin/python
 
 import subprocess
 import os
 homeSize = subprocess.Popen("du -hs /home/$USER", shell=True, stdout=subprocess.PIPE).stdout.read().split()[0]
 
-if homeSize[-1] == "G" and homeSize[:-1] >= "200":
+if homeSize[-1] == "G" and int(homeSize[:-1]) >= 200:
 	os.system('echo "$(tput setab 1)You have %s of data in your HOME directory.$(tput sgr 0)"' % homeSize)
 	os.system('echo "$(tput setab 1)Please decrease your disk usage to <200G.$(tput sgr 0)"')
 
